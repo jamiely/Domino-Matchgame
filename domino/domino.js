5913088cc008f0146891c930cab8e16a2722e0dc
Jamie Ly <jamiely@dhcp163-21.wharton.upenn.edu>
2011-02-12 22:15:51 -0500

    Added compiled js

--- domino/domino.js
+++ domino/domino.js
@@ -6,12 +6,7 @@ goog.provide('domino');
 goog.require('lime.Director');
 goog.require('lime.Scene');
 goog.require('lime.Layer');
-goog.require('lime.Circle');
 goog.require('lime.Label');
-goog.require('lime.animation.Spawn');
-goog.require('lime.animation.FadeTo');
-goog.require('lime.animation.ScaleTo');
-goog.require('lime.animation.MoveTo');
 
 domino.blockWidth = 30;
 domino.blockHeight = 30;
